2f793a2d9005f738f68700671a6bc8e64882ea4c
Petr Mašát <petr.masat@onlio.com>
2013-06-26 00:53:35 +0200

    Základ na novinky

---
+++ app/model/TestRepository.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Aiki;
+Use Nette;
+
+
+class NewsRepository extends Repository {
+	
+	public function findLast() {
+		//return $this->findBy(array('done' => false))->order('created ASC');
+		
+	}
+}
--- app/presenters/HomepagePresenter.php
+++ app/presenters/HomepagePresenter.php
@@ -3,12 +3,20 @@
 /**
  * Homepage presenter.
  */
-class HomepagePresenter extends BasePresenter
-{
+class HomepagePresenter extends BasePresenter {
+	private $newsRepository;
+	
+	protected function startup() {
+		parent::startup();
+		$this->newsRepository = $this->context->newsRepository;
+	}
+	public function beforeRender(){
+		$this->template->news = $this->newsRepository->findAll();
+	}
 
-	public function renderDefault()
-	{
+	public function renderDefault() {
 		$this->template->anyVariable = 'any value';
+		
 	}
 
 }
