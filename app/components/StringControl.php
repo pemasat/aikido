aff1c53acc69f515c65ecf672ed865f308122a41
Petr Mašát <petr.masat@onlio.com>
2014-04-17 13:03:36 +0200

    Editační formulář v strirn attributu

--- app/components/StringControl.php
+++ app/components/StringControl.php
@@ -1,6 +1,8 @@
 <?php
 
 use Nette\Application\UI;
+use Nette\Application\UI\Form;
+
 
 /**
  * The attribute String control
@@ -29,7 +31,7 @@ class StringControl extends UI\Control {
 	}
 	
 	public function handleEdit($param) {
-		$this->stringAttribute = $this->presenter->context->stringAttribute;
+		$this->stringAttribute = $this->presenter->context->stringAttribute;		
 		Nette\Diagnostics\FireLogger::log($this->params['uri']. '::' . $this->params['key']);
 		Nette\Diagnostics\FireLogger::log($this->stringAttribute->getValue($param['uri'], $param['key']));
 		$template = $this->template;
@@ -38,6 +40,28 @@ class StringControl extends UI\Control {
 		$template->render();
 	}
 
+	protected function createComponentEditForm($name) {
+		 $form = new Form($this, $name);
+		 $form->addText('value', 'Hodnota');
+		 $form->addSubmit('send', 'Uložit');
+		 $form->onSuccess[] = $this->editFormSubmitted;
+		 return $form;
+	}
+	public function editFormSubmitted(Form $form) {
+		/*
+		$this->stringAttribute->setValue(
+			$form->values->
+		);
+		$this->personsRepository->create(
+			$form->values->title,
+			$form->values->level,
+			$form->values->perex,
+			$form->values->content
+		);
+		$this->flashMessage('Uživatel byl založen');
+		$this->redirect('Persons:');
+	 */
+	}
 
 
 }
